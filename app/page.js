e68ce956da43ce74ffb150c171aa3c69b12dba90
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-06-06 21:51:32 -0500

    modifico la landing agregando las cards y serachbar (aun falta modificar cosas)

--- app/page.js
+++ app/page.js
@@ -1,22 +1,18 @@
-
-import Link from 'next/link';
 import fetchData from './lib/API';
+import Cards from './ui/home/cards';
+import NavBar from './ui/navbar';
+import SearchBar from './ui/search';
 
 export default async function Home() {
-  const agents = await fetchData()  
-
+  const agents = await fetchData()
+  const filteredAgents = agents.filter((agent) => agent.isPlayableCharacter)
+  const filteredRole = filteredAgents.filter((agentFiltered) => agentFiltered.role.uuid === '1b47567f-8f7b-444b-aae3-b0c634622d10' || '5fc02f99-4091-4486-a531-98459a3e95e9' || 'dbe8757e-9e92-4ed4-b39f-9dfc589691d4' || '4ee40330-ecdd-4f2f-98a8-eb1243428373' )
+  
   return (
-    <div>
-      <h2>Lista de agentes</h2>
-      {
-        agents.map((agent) => (
-          <div key={agent.uuid}>
-            <Link href={`/${agent.uuid}`}>{agent.displayName}</Link>
-            <h2>{agent.description}</h2>
-            <img src={agent.displayIcon} alt={agent.displayName} width={200} />
-          </div>
-        ))
-      }
+    <div className='flex flex-col mb-20'>
+      <SearchBar/>
+      <h2 className='hidden md:block'>Lista de agentes</h2>
+      <Cards filteredRole={filteredRole}/>
     </div>
   );
 }
