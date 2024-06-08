64cc00bba06c3d8efa5ecc47661567622fcac07c
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-06-07 19:25:34 -0500

    cards modificadas para que pueda filtrar

--- app/ui/home/cards.jsx
+++ app/ui/home/cards.jsx
@@ -1,12 +1,22 @@
 import Link from "next/link";
 import Card from "./card";
 
-export default function Cards({ filteredRole }) {
+export default function Cards({ filteredRole, query, filterRole }) {
+    const lowerCaseQuery = query.toLowerCase();
+
+    // Primero aplicamos el filtro de búsqueda
+    const searchAgent = lowerCaseQuery === '' ? filteredRole
+        : filteredRole.filter((agent) => agent.displayName.toLowerCase().includes(lowerCaseQuery));
+
+    // Luego aplicamos el filtro de rol sobre los resultados de la búsqueda
+    const filteredAgents = filterRole === '' ? searchAgent 
+        : searchAgent.filter((agent) => agent.role.uuid.includes(filterRole));
+
     return (
         <div className="grid grid-cols-2 mx-auto md:grid-cols-4 xl:grid-cols-6 md:space-x-2">
             {
-                filteredRole.map(({ uuid, displayName, fullPortrait, role, backgroundGradientColors}) => (
-                    <Link href={`/${uuid}`}>
+                filteredAgents.map(({ uuid, displayName, fullPortrait, role, backgroundGradientColors }) => (
+                    <Link href={`/${uuid}`} key={uuid}>
                         <Card
                             id={uuid}
                             name={displayName}
@@ -17,7 +27,6 @@ export default function Cards({ filteredRole }) {
                     </Link>
                 ))
             }
-
         </div>
-    )
+    );
 }
