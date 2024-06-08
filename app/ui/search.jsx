8014e828634a1e7f582d7d53478095f3a606cb77
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-06-07 19:25:21 -0500

    buscador funcional

--- app/ui/search.jsx
+++ app/ui/search.jsx
@@ -1,12 +1,34 @@
+'use client'
 import { MdSearch } from "react-icons/md";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
-export default function SearchBar() {
+export default function SearchBar({ placeholder }) {
+    const searchParams = useSearchParams()
+    const pathname = usePathname()
+    const { replace } = useRouter()
+
+    const handleSearch = useDebouncedCallback((event) => {
+        const params = new URLSearchParams(searchParams)
+        if (event) {
+            params.set('query', event)
+        } else {
+            params.delete('query')
+        }
+        replace(`${pathname}?${params.toString()}`)
+        console.log(event);
+    },300)
     return (
-        <div className="mx-auto relative my-10">
+        <div className="mx-auto md:mx-0 relative my-10 ">
             <div className="absolute">
                 <MdSearch className="absolute inset-x-[330px] right-0 pr-3 flex items-center pointer-events-none text-gray-400 w-10 h-10" />
             </div>
-            <input type="text" placeholder="Buscar agente..." className="px-2 w-[370px] h-9 rounded-md text-black" />
+            <input type="text"
+                className="peer block pl-5 w-[370px] h-9 rounded-md text-sm text-black "
+                placeholder={placeholder}
+                onChange={(event) => { handleSearch(event.target.value) }}
+                defaultValue={searchParams.get('query')?.toString()}
+            />
         </div>
     )
 }
