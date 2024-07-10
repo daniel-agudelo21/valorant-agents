91a6b5c540655d5baad85a0f4754087c38107b80
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-07-09 19:28:11 -0500

    seccion de las habilidades modificada y habilitada, falta organizar el disenio

--- app/ui/detail/abilities.jsx
+++ app/ui/detail/abilities.jsx
@@ -1,24 +1,35 @@
 "use client"
-export default function Abilities({ agent }) {
-    const color = agent.backgroundGradientColors[1]
-    const handleAbility = (slot) => {
+import { useState } from 'react';
+import { outfit } from '../fonts';
 
-    }
+export default function Abilities({ agent }) {
+    const [selectedAbility, setSelectedAbility] = useState(null);
+    const bgColor = `#${agent.backgroundGradientColors[1]}`;
+
+    const handleAbility = (ability) => {
+        setSelectedAbility(ability);
+    };
+
     return (
-        <div>
-            {
-                agent.abilities.map((ability) => (
-                    <article key={ability.slot}>
-                        <button className="border rounded-lg p-1"
-                            onClick={() => { handleAbility(ability.slot) }}
-                        >
-                            <img src={ability.displayIcon} alt={ability.displayName} width={45} />
-                        </button>
-                        <h3 className={`text-[#${color}]`}>{ability.displayName}</h3>
-                        <p>{ability.description}</p>
-                    </article>
-                ))
-            }
+        <div className="flex flex-col  mx-[42px] mb-40 mt-4 ">
+            <div className="flex gap-4 ">
+                {agent.abilities.map((ability) => (
+                    <button
+                        key={ability.slot}
+                        className='border rounded-lg p-1 z-50'
+                        style={selectedAbility === ability ? { backgroundColor: bgColor } : {}}
+                        onClick={() => handleAbility(ability)}
+                    >
+                        <img src={ability.displayIcon} alt={ability.displayName} width={62} />
+                    </button>
+                ))}
+            </div>
+            {selectedAbility && (
+                <div className="mt-8  z-50">
+                    <h3 className={` ${outfit.className} text-2xl font-bold`} style={{ color: bgColor }}>{selectedAbility.displayName}</h3>
+                    <p className='mt-2'>{selectedAbility.description}</p>
+                </div>
+            )}
         </div>
-    )
+    );
 }
