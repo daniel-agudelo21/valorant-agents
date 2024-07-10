68f5afea92f7234aa775daeee5b9ad679a087b60
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-07-09 19:28:27 -0500

    disenio mobile ajustado ya

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -16,23 +16,26 @@ export default async function Detail({ params }) {
                 </svg>
                 <div className="absolute flex h-[550px] lg:h-[700px] 2xl:w-[900px] xl:h-[900px] mt-4 justify-center overflow-hidden blur-[3px] ">
                     {/* imagen del bg */}
-                    <img src={agent.background} alt={agent.displayName} className="hidden md:flex object-none scale-105 drop-shadow-[10px_2px_10px_rgba(0,0,0,1)]" />
+                    <img src={agent.background} alt={agent.displayName} className="hidden md:flex object-none lg:object-cover xl:object-none scale-105 drop-shadow-[10px_2px_10px_rgba(0,0,0,1)]" />
                 </div>
                 {/* icono del rol */}
-                <img src={agent.role.displayIcon} alt={agent.role.displayName} width={82} className="absolute left-[78%] bottom-[78%] drop-shadow-[2px_2px_2px_rgba(0,0,0,1)]" />
+                <img src={agent.role.displayIcon} alt={agent.role.displayName} width={82} className="absolute hidden lg:block lg:w-[82px] lg:left-[83%] lg:top-24 2xl:left-[78%] 2xl:bottom-[78%] drop-shadow-[2px_2px_2px_rgba(0,0,0,1)]" />
                 {/* imagen del personaje */}
                 <img src={agent.bustPortrait} alt={agent.displayName} className="hidden md:flex absolute drop-shadow-[2px_2px_2px_rgba(0,0,0,1)]" />
-                <img src={agent.bustPortrait} alt={agent.displayName} width={496} height={496} className="flex md:hidden" />
+                <img src={agent.bustPortrait} alt={agent.displayName} width={496} height={496} className="flex items-end md:hidden drop-shadow-[2px_2px_2px_rgba(0,0,0,1)]" />
                 {/* nombre y rol */}
-                <div className="absolute bottom-0 2xl:left-[260px] 2xl:-bottom-0   rounded-2xl p-6 z-30">
-                    <h2 className={`${outfit.className} font-semibold text-3xl z-10 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]`}>{agent.role.displayName}</h2>
-                    <h1 className='text-[88px] font-cyber z-10 drop-shadow-[8px_2px_2px_rgba(0,0,0,1)]' style={{ color: textColor }}>{agent.displayName}</h1>
+                <div className="absolute top-[30%] left-8 lg:left-3 lg:top-[40%] xl:left-8 xl:top-[68%] 2xl:left-[260px] 2xl:top-[78%] lg:p-6 z-30">
+                    <div className="flex flex-row items-center gap-x-4">
+                        <h2 className={`${outfit.className} text-[21px] font-semibold lg:text-2xl 2lg:text-3xl z-10 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]`}>{agent.role.displayName}</h2>
+                        <img src={agent.role.displayIcon} alt={agent.role.displayName} width={22} className="block md:hidden drop-shadow-[5px_2px_2px_rgba(0,0,0,1)] " />
+                    </div>
+                    <h1 className='text-[53px] lg:text-[55px] 2lg:text-[88px] font-cyber z-10 drop-shadow-[8px_2px_2px_rgba(0,0,0,1)]' style={{ color: textColor }}>{agent.displayName}</h1>
                 </div>
             </div>
             <div>
                 <h2 className="hidden">{agent.description}</h2>
-                <h3>Habilidades</h3>
-                {/* <Abilities agent={agent} /> */}
+                <h3 className={`${outfit.className} text-[23px] font-bold mx-[42px]`}>Habilidades</h3>
+                <Abilities agent={agent} />
             </div>
         </div>
     )
