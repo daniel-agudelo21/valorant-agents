154716c4de07fc353e1b40b22345e61c55cc1b4e
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-07-11 18:57:47 -0500

    agrego un espacio cuando la vista es mayor a 768px

--- app/ui/home/cards.jsx
+++ app/ui/home/cards.jsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
 import Card from "./card";
 
-export default function Cards({ filteredRole, query, filterRole }) {
+export default function Cards({ playableAgents, query, filterRole }) {
     const lowerCaseQuery = query.toLowerCase();
 
     // Primero aplicamos el filtro de búsqueda
-    const searchAgent = lowerCaseQuery === '' ? filteredRole
-        : filteredRole.filter((agent) => agent.displayName.toLowerCase().includes(lowerCaseQuery));
+    const searchAgent = lowerCaseQuery === '' ? playableAgents
+        : playableAgents.filter((agent) => agent.displayName.toLowerCase().includes(lowerCaseQuery));
 
     // Luego aplicamos el filtro de rol sobre los resultados de la búsqueda
     const filteredAgents = filterRole === '' ? searchAgent 
         : searchAgent.filter((agent) => agent.role.uuid.includes(filterRole));
 
     return (
-        <div className="grid grid-cols-2 mx-auto md:grid-cols-4 xl:grid-cols-6 md:space-x-2">
+        <div className="grid grid-cols-2 mx-auto md:grid-cols-4 xl:grid-cols-6 md:space-x-2 md:mt-16">
             {
                 filteredAgents.map(({ uuid, displayName, fullPortrait, role, backgroundGradientColors }) => (
                     <Link href={`/${uuid}`} key={uuid}>
