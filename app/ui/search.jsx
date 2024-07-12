138104683592e6435740645b9ed9149cfcfb1bbb
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-07-11 19:00:14 -0500

    modifico el search para ajustarlo al mobile

--- app/ui/search.jsx
+++ app/ui/search.jsx
@@ -17,14 +17,14 @@ export default function SearchBar({ placeholder }) {
         }
         replace(`${pathname}?${params.toString()}`)
         console.log(event);
-    },300)
+    }, 300)
     return (
-        <div className="mx-auto md:mx-0 relative my-10 ">
+        <div className="mx-auto md:mx-0 relative my-10  max-w-[250px]">
             <div className="absolute">
-                <MdSearch className="absolute inset-x-[330px] right-0 pr-3 flex items-center pointer-events-none text-gray-400 w-10 h-10" />
+                <MdSearch className="absolute inset-x-[220px] lg:inset-x-[330px] lg:right-0 pr-3 flex items-center pointer-events-none text-gray-400 w-10 h-10" />
             </div>
             <input type="text"
-                className="peer block pl-5 w-[370px] h-9 rounded-md text-sm text-black "
+                className="peer block pl-5 w-[250px] lg:w-[370px] h-9 rounded-md text-sm text-black "
                 placeholder={placeholder}
                 onChange={(event) => { handleSearch(event.target.value) }}
                 defaultValue={searchParams.get('query')?.toString()}
