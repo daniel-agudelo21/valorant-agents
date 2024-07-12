7ac9e049719ce0b905fe69b963e9728be45c1d23
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-07-12 12:31:39 -0500

    cambio el metadata

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,17 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./ui/navbar";
 import Footer from "./ui/footer";
+import { inter } from "./ui/fonts";
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata = {
-  title: "Agentes de Valorant",
+  title: {
+    template: '%s | Valorant',
+    default: 'Agentes de Valorant'
+  },
   description: "Agentes del juego Valorant de RIOT GAMES",
+  metadataBase: new URL('https://playvalorant.com/en-us/media/')
 };
 
 export default function RootLayout({ children }) {
