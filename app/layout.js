b9ad3dc406b45eb51be0df096473fc86c908e82e
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-07-11 19:00:56 -0500

    agrego el footer y modifico las dimensiones del body para que el footer se ajuste bien.

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,24 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./ui/navbar";
+import Footer from "./ui/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Valorant agents",
+  title: "Agentes de Valorant",
   description: "Agentes del juego Valorant de RIOT GAMES",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={`${inter.className} bg-[#131215] text-white`}>
+      <body className={`${inter.className} bg-[#131215] text-white h-full flex flex-col min-h-screen`}>
         <NavBar />
-        {children}
+        <div className="flex-1">
+          {children}
+        </div>
+        <Footer />
       </body>
     </html>
   );
